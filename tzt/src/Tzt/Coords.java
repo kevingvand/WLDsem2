12837885fb7a85b364ae6c5feda0658a01fbe4dc
Kevin van Dommele <kevingvand@gmail.com>
2013-05-13 00:42:48 +0200

    test
    test

---
+++ tzt/src/Tzt/Coords.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Tzt;
+
+/**
+ *
+ * @author kevinvandommele
+ */
+public class Coords {
+    private double latitude;
+    private double longitude;
+    
+    public Coords(double lat, double long){
+        this.latitude = lat;
+        this.longitude = long;
+    }
+}
